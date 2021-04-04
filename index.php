c1f62fab9585ebeecec5e9b083fdb93e82abd973
Rob Nugen <rob@robnugen.com>
2021-04-04 19:19:13 +0900

    First draft sending multiple pictures
    but on the receiving side, they are a mess:
    
    [pictures] => Array
            (
                [name] => Array
                    (
                        [1] => pp02.png
                        [2] => pp05.png
                    )
    
                [type] => Array
                    (
                        [1] => image/png
                        [2] => image/png
                    )
    
                [tmp_name] => Array
                    (
                        [1] => /tmp/php56D9UV
                        [2] => /tmp/phpmDW6EH
                    )
    
                [error] => Array
                    (
                        [1] => 0
                        [2] => 0
                    )
    
                [size] => Array
                    (
                        [1] => 46954
                        [2] => 47401
                    )
    
            )
    
    so eff parsing that.

--- index.php
+++ index.php
@@ -2,6 +2,9 @@ Hello
 
 <form method="POST" action="bullet.php" enctype="multipart/form-data" >
   <input type="hidden" name="MAX_FILE_SIZE" value="5000000"/>
-  <input type="file" name="pictures" />
+  <input type="file" name="pictures[1]" />
+  <input type="text" name="image_name[1]" />
+  <input type="file" name="pictures[2]" />
+  <input type="text" name="image_name[2]" />
   <input type="submit" value="upload"/>
 </form>
