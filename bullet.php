478afb7d6a5bb0a88609bce6dbe53630ef3a0778
Rob Nugen <rob@robnugen.com>
2021-04-03 23:23:12 -0700

    First draft saved images to b.robnugen.com/blog yay!

---
+++ bullet.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once  "/home/thundergoblin/bulletproof/src/bulletproof.php";
+
+$image = new Bulletproof\Image($_FILES);
+
+  $image->setName("honkey")
+        ->setMime(["png","gif","jpg"])
+        ->setStorage("/home/thundergoblin/b.robnugen.com/blog/");
+
+if($image["pictures"]){
+  $upload = $image->upload(); 
+
+  if($upload){
+    echo "hellolololo";
+    echo $upload->getFullPath(); // uploads/cat.gif
+    echo $image->getName(); // samayo
+    echo $image->getMime(); // gif
+    echo $image->getLocation(); // avatars
+    echo $image->getFullPath(); // avatars/samayo.gif
+  }else{
+    echo "errrrrored";
+    echo $image->getError(); 
+  }
+}
+
+  ?>
+
+brace yourself, fool.
---
+++ index.php
@@ -0,0 +1,7 @@
+Hello
+
+<form method="POST" action="bullet.php" enctype="multipart/form-data" >
+  <input type="hidden" name="MAX_FILE_SIZE" value="1000000"/>
+  <input type="file" name="pictures" />
+  <input type="submit" value="upload"/>
+</form>
