86845c9ac72c245576120a1bca980ef30d541a2d
Rob Nugen <rob@robnugen.com>
2021-04-05 09:33:26 +0900

    Send up to six images at a time

--- index.php
+++ index.php
@@ -2,9 +2,29 @@ Hello
 
 <form method="POST" action="bullet.php" enctype="multipart/form-data" >
   <input type="hidden" name="MAX_FILE_SIZE" value="5000000"/>
-  <input type="file" name="pictures[1]" />
-  <input type="text" name="image_name[1]" />
-  <input type="file" name="pictures[2]" />
-  <input type="text" name="image_name[2]" />
+  <div>
+    <input type="file" name="pictures1" />
+    <input type="text" name="image_name[1]" />
+  </div>
+  <div>
+    <input type="file" name="pictures2" />
+    <input type="text" name="image_name[2]" />
+  </div>
+  <div>
+    <input type="file" name="pictures3" />
+    <input type="text" name="image_name[3]" />
+  </div>
+  <div>
+    <input type="file" name="pictures4" />
+    <input type="text" name="image_name[4]" />
+  </div>
+  <div>
+    <input type="file" name="pictures5" />
+    <input type="text" name="image_name[5]" />
+  </div>
+  <div>
+    <input type="file" name="pictures6" />
+    <input type="text" name="image_name[6]" />
+  </div>
   <input type="submit" value="upload"/>
 </form>
