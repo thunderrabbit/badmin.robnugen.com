0b2875a193aef9a5f5bdf28baee4383c39618daf
Rob Nugen <rob@robnugen.com>
2021-04-05 09:33:12 +0900

    Use image name if name does not exist

--- bullet.php
+++ bullet.php
@@ -15,48 +15,34 @@ function print_rob($object, $exit = true) {
     }
 }
 
+function create_image_name($image_name, $image_info)
+{
+  $return_val = $image_name ? $image_name : pathinfo($image_info['name'], PATHINFO_FILENAME);
+  return $return_val;
+}
 $images = new Bulletproof\Image($_FILES);
 
-print_rob($_REQUEST,0);
 // We can easily loop through array $_POST['image_name']
 // but cannot as easily loop through an array of $_FILES['pictures']  (see commit c1f62fab9585ebeecec5)
 foreach($_POST['image_name'] as $key => $image_name)
 {
-  print_rob("key " . $key,0);
-  print_rob("image name: ". $image_name,0);
-  print_rob($_FILES["pictures".$key],0);
-  print_rob("about to get key " . "pictures".$key,0);
   if($images["pictures".$key]){
-    print_rob("key be pictures".$key,0);
-    $images->setName($image_name)
-          ->setStorage("/home/thundergoblin/b.robnugen.com/tmp");  // no trailing slash
+    $save_image_name = create_image_name($image_name,$_FILES["pictures".$key]);
+    $images->setName($save_image_name)
+           ->setStorage("/home/thundergoblin/b.robnugen.com/tmp");  // no trailing slash
 
     $upload = $images->upload();
-    print_rob("hellolololo" . __LINE__,0);
     print_rob($upload->getPath(),0);
+  } else if(!empty($save_image_name)) {
+    // (There was an image name but no file)
+    echo "<br>apparently nothing in images[\"pictures\".$key]";
+    echo "<br>so what about files?<br>";
+    print_rob($_FILES["pictures".$key]);
   }
 }
 print_rob("ai");
 
 
-if($images["pictures"]){
-  $upload = $images->upload();
-
-  if($upload){
-    echo $upload->getFullPath(); // uploads/cat.gif
-    echo $image->getName(); // samayo
-    echo $image->getMime(); // gif
-    echo $image->getLocation(); // avatars
-    echo $image->getFullPath(); // avatars/samayo.gif
-  }else{
-    echo "errrrrored";
-    echo $images->getError();
-  }
-} else {
-  echo "<br>apparently nothing in image['pictures']";
-  echo "<br>so what about files?<br>";
-  print_rob($_FILES);
-}
 
   ?>
 
